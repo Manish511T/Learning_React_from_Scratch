681f37e3a3201782bb7e96ce3e07519d027d7ef4
Manish <mkthakur2301@gmail.com>
2026-01-22 14:30:48 +0530

    Episode 7 part 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
   // let btn = "Login"
   const [btn, setBtn] = useState("Login")
+  
+  // if no dependency array => useEffect is called on every renders
+  // if dependency array is empty => [] useEffect is called on initial render(just once).
+  // if dependency  array is [btn] => called everytime btn is updated
+  useEffect(()=>{
+    console.log("useEffect called")
+  }, []);
 
   return (
     <div className="header">
