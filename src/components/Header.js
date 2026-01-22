0da4885caabee919fdac324b238479a06726c83e
Manish <mkthakur2301@gmail.com>
2026-01-22 16:25:59 +0530

    Episode 7 part 3 spa

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   // let btn = "Login"
@@ -18,13 +19,19 @@ const Header = () => {
         <img
           className="logo"
           src={LOGO_URL}
-        />
+        /> 
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
           <li>Cart</li>
           <button onClick={()=>{btn==="Login"?setBtn("Logout"):setBtn("Login")}} className="login">{btn}</button>
         </ul>
