3db1bea7ca85c71f1a3d1bd69b5d42caa05b406e
Manish <mkthakur2301@gmail.com>
2026-02-08 00:39:59 +0530

    Unit test

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,14 @@
 const Contact = ()=>{
     return(
         <div>
-            Contact Us Page
+            <h1 className="font-bold text-3xl p-4 m-4">Contact Us Page</h1>
+            <form>
+                <input type="text" className="border-black border p-2 m-2" placeholder="name"/>
+                <input type="text" className="border-black border p-2 m-2" placeholder="message"/>
+                <button className="border-black border p-2 m-2 rounded-lg bg-gray-200">
+                    Submit
+                </button>
+            </form>
         </div>
     )
 }
