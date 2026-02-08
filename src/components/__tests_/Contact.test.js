d8f171fea6f53987a114e4e4bcee704fc32bd3fe
Manish <mkthakur2301@gmail.com>
2026-02-08 23:35:47 +0530

    Test equal to it

--- src/components/__tests_/Contact.test.js
+++ src/components/__tests_/Contact.test.js
@@ -3,7 +3,7 @@ import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
 describe("Contact us page Test cases", () => {
-  test("Should load contact us component", () => {
+  it("Should load contact us component", () => {
     render(<Contact />);
 
     const heading = screen.getByRole("heading");
@@ -11,7 +11,7 @@ describe("Contact us page Test cases", () => {
     expect(heading).toBeInTheDocument();
   });
 
-  test("Should load Button inside my contact component", () => {
+  it("Should load Button inside my contact component", () => {
     render(<Contact />);
 
     const button = screen.getByRole("button");
@@ -19,19 +19,20 @@ describe("Contact us page Test cases", () => {
     expect(button).toBeInTheDocument();
   });
 
-  test("Should load input name on the contact component", () => {
+  it("Should load input name on the contact component", () => {
     render(<Contact />);
 
     const inputName = screen.getByPlaceholderText("name");
 
     expect(inputName).toBeInTheDocument();
   });
+  it("Should load 2 input boxes on the  contact component", () => {
+    render(<Contact />);
+
+    const inputBoxes = screen.getAllByRole("textbox");
+    console.log(inputBoxes);
+    expect(inputBoxes.length).toBe(2);
+  });
 });
 
-test("Should load 2 input boxes on the  contact component", () => {
-  render(<Contact />);
-
-  const inputBoxes = screen.getAllByRole("textbox");
-  console.log(inputBoxes);
-  expect(inputBoxes.length).toBe(2);
-});
+// Test == it
