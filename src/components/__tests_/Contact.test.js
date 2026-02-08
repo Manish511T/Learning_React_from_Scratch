daa3d06ebf2f9996d6fc9bc15ebf646c0fba6c32
Manish <mkthakur2301@gmail.com>
2026-02-08 23:33:14 +0530

    Contact us page testing

--- src/components/__tests_/Contact.test.js
+++ src/components/__tests_/Contact.test.js
@@ -1,11 +1,37 @@
-import { render, screen } from "@testing-library/react"
-import Contact from "../Contact"
-import "@testing-library/jest-dom"
+import { render, screen } from "@testing-library/react";
+import Contact from "../Contact";
+import "@testing-library/jest-dom";
 
-test("Should load contact us component", ()=>{
-    render(<Contact/>)
+describe("Contact us page Test cases", () => {
+  test("Should load contact us component", () => {
+    render(<Contact />);
 
-    const heading = screen.getByRole("heading")
+    const heading = screen.getByRole("heading");
 
     expect(heading).toBeInTheDocument();
-})
+  });
+
+  test("Should load Button inside my contact component", () => {
+    render(<Contact />);
+
+    const button = screen.getByRole("button");
+
+    expect(button).toBeInTheDocument();
+  });
+
+  test("Should load input name on the contact component", () => {
+    render(<Contact />);
+
+    const inputName = screen.getByPlaceholderText("name");
+
+    expect(inputName).toBeInTheDocument();
+  });
+});
+
+test("Should load 2 input boxes on the  contact component", () => {
+  render(<Contact />);
+
+  const inputBoxes = screen.getAllByRole("textbox");
+  console.log(inputBoxes);
+  expect(inputBoxes.length).toBe(2);
+});
